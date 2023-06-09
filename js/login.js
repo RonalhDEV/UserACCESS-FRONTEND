ad8c8274ce149df8ea79bb6ea15d7112473ac057
Ronaldo <arolpersonal@gmail.com>
2023-06-09 16:55:17 -0500

    Se comentó el código

--- js/login.js
+++ js/login.js
@@ -1,15 +1,21 @@
-const url = "http://localhost:8080/api/user"
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-  event.preventDefault();
+// Definición de la URL del endpoint de la API
+const url = "http://localhost:8080/api/user";
 
+// Escuchar el evento de envío del formulario de inicio de sesión
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+
+  // Obtener los valores de los campos de entrada de correo electrónico y contraseña
   var emailLog = document.getElementById("emailInput").value;
   var passwordLog = document.getElementById("passwordInput").value;
 
+  // Crear un objeto JSON con los valores de correo electrónico y contraseña
   var logJson = {
     emailUsuario: emailLog,
     passwordUsuario: passwordLog
   };
 
+  // Realizar una solicitud de inicio de sesión al servidor
   fetch(`${url}/login`, {
     method: "POST",
     headers: {
@@ -18,6 +24,7 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     body: JSON.stringify(logJson)
   })
     .then(function (response) {
+      // Manejar la respuesta de la solicitud
       if (response.ok) {
         return response.text();
       } else if (response.status === 404) {
@@ -29,10 +36,12 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
       }
     })
     .then(function (responseData) {
+      // Manejar los datos de respuesta y redirigir a otra página
       window.location.href = "index.html";
       alert("¡Logeo exitoso!");
     })
     .catch(function (error) {
+      // Capturar y mostrar cualquier error ocurrido durante el proceso
       console.error(error);
       alert(error.message);
     });
--- js/register.js
+++ js/register.js
@@ -1,15 +1,21 @@
+// Definición de la URL del endpoint de la API
 const url = "http://localhost:8080/api/user";
-document.getElementById("regForm").addEventListener("submit", function (event) {
-  event.preventDefault();
 
+// Escuchar el evento de envío del formulario de registro
+document.getElementById("regForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+
+  // Obtener los valores de los campos de entrada de correo electrónico y contraseña
   var emailReg = document.getElementById("emailInput").value;
   var passwordReg = document.getElementById("passwordInput").value;
 
+  // Crear un objeto JSON con los valores de correo electrónico y contraseña
   var regJson = {
     emailUsuario: emailReg,
     passwordUsuario: passwordReg
   };
 
+  // Realizar una solicitud de registro al servidor
   fetch(`${url}/crear`, {
     method: "POST",
     headers: {
@@ -18,6 +24,7 @@ document.getElementById("regForm").addEventListener("submit", function (event) {
     body: JSON.stringify(regJson)
   })
     .then(function (response) {
+      // Manejar la respuesta de la solicitud
       if (response.ok) {
         return response.text();
       } else {
@@ -25,11 +32,12 @@ document.getElementById("regForm").addEventListener("submit", function (event) {
       }
     })
     .then(function (responseData) {
-        window.location.href = "login.html";
-        alert("Registro exitoso. ¡Ahora inicia sesión!");
-
+      // Manejar los datos de respuesta y redirigir a la página de inicio de sesión
+      window.location.href = "login.html";
+      alert("Registro exitoso. ¡Ahora inicia sesión!");
     })
     .catch(function (error) {
+      // Capturar y mostrar cualquier error ocurrido durante el proceso de registro
       console.error(error);
       alert("Ocurrió un error durante el registro. Por favor, intenta nuevamente.");
     });
